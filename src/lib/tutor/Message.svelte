79ee41476b6ba273875137b687aaa8068dcd20ce
Arnav <arnard76@gmail.com>
2025-10-09 12:28:49 +1300

    fix formatting of tutor replies

--- src/lib/tutor/Message.svelte
+++ src/lib/tutor/Message.svelte
@@ -3,20 +3,23 @@
 	import type { MessageType } from './message';
 	import { page } from '$app/stores';
 	import CopyToClipboard from '$lib/components/CopyToClipboard.svelte';
-	let { message, children }: { message: MessageType; children?: Snippet<[]> } = $props();
+	import { marked } from 'marked';
 
+	let { message, children }: { message: MessageType; children?: Snippet<[]> } = $props();
 	let copyWithClick = $derived($page.url.href.includes('wizard-of-oz'));
 </script>
 
-<p
-	class="flex rounded-lg px-4 py-1 whitespace-pre-wrap backdrop-blur-2xl {message.from === 'tutor'
+<div
+	class="flex rounded-lg px-4 py-1 whitespace-pre-wrap text-black backdrop-blur-2xl {message.from ===
+	'tutor'
 		? 'mr-16 self-start rounded-bl-none bg-blue-400 font-mono'
 		: 'ml-16 self-end rounded-br-none bg-white'}"
 >
-	{#if copyWithClick && message.message}
-		<CopyToClipboard value={message.message} />
-	{:else}
-		{message.message}
-	{/if}
+	<div class="prose mb-0 flex h-min flex-col pb-0">
+		{@html marked(message.message)}
+	</div>
 	{@render children?.()}
-</p>
+	{#if copyWithClick && message.message}
+		<CopyToClipboard value={message.message}>|</CopyToClipboard>
+	{/if}
+</div>
