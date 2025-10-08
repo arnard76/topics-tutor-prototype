6e22f83f89e272772510bf50e2df684d8f2f2875
Arnav <arnard76@gmail.com>
2025-10-08 22:44:49 +1300

    improve layout and styling of reading

--- src/routes/reading-material/+page.svelte
+++ src/routes/reading-material/+page.svelte
@@ -3,10 +3,7 @@
 	import { chunkProgress, readingMaterialChunks } from '$lib/reading/reading-material';
 </script>
 
-<div class="flex h-screen w-screen flex-col bg-reading-bg">
-	<div class="flex justify-end">
-		<a href="/tutor" class="border-white text-nowrap">Ask tutor</a>
-	</div>
+<div class="flex h-screen w-screen flex-col bg-reading-bg pt-8">
 	<div class="flex h-full flex-1 flex-col gap-8 overflow-y-auto scroll-smooth">
 		{#each $readingMaterialChunks.slice(0, $chunkProgress + 1) as readingChunk, i (readingChunk.id)}
 			<div class="mx-auto w-[80%] text-2xl text-white" id="chunk-{i}">
@@ -14,7 +11,7 @@
 			</div>
 		{/each}
 	</div>
-	<div class="flex justify-end">
+	<div class="flex justify-end gap-4">
 		<button
 			class="border-white text-nowrap"
 			onclick={() => {
@@ -22,13 +19,14 @@
 				goto('/reading-material#chunk-' + ($chunkProgress - 1));
 			}}>Read more</button
 		>
-		<a class="border-white text-nowrap" href="/done">Finish Reading</a>
+		<a href="/tutor" class="border-white text-nowrap">Ask tutor</a>
+		<a class="border-white text-nowrap" href="/done">Finish reading</a>
 	</div>
 </div>
 
 <style>
 	a,
 	button {
-		color: darkslategray;
+		color: gray;
 	}
 </style>
