00236fd5912e7fd695666c7b48200f52f57022cc
Arnav <arnard76@gmail.com>
2025-10-13 21:10:16 +1300

    improve formatting for actual story

--- src/routes/reading-material/+page.svelte
+++ src/routes/reading-material/+page.svelte
@@ -1,13 +1,14 @@
 <script>
 	import { goto } from '$app/navigation';
+	import { marked } from 'marked';
 	import { chunkProgress, readingMaterialChunks } from '$lib/reading/reading-material';
 </script>
 
 <div class="flex h-screen w-screen flex-col bg-reading-bg pt-8">
-	<div class="flex h-full flex-1 flex-col gap-8 overflow-y-auto scroll-smooth">
+	<div class="flex h-full flex-1 flex-col gap-10 overflow-y-auto scroll-smooth whitespace-pre-wrap">
 		{#each $readingMaterialChunks.slice(0, $chunkProgress + 1) as readingChunk, i (readingChunk.id)}
-			<div class="mx-auto w-[80%] text-2xl text-white" id="chunk-{i}">
-				{readingChunk.content}
+			<div class="mx-auto w-[80%] text-xl whitespace-pre-wrap text-white" id="chunk-{i}">
+				{@html marked(readingChunk.content)}
 			</div>
 		{/each}
 	</div>
